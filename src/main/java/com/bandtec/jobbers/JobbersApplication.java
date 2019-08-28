e45c050ea37cbf953aec65abb023c977af6c5d53
Rodrigo <rod.bra.san@gmail.com>
2019-08-28 18:14:58 -0300

    primeiro push

---
+++ src/main/java/com/bandtec/jobbers/JobbersApplication.java
@@ -0,0 +1,13 @@
+package com.bandtec.jobbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobbersApplication.class, args);
+    }
+
+}
