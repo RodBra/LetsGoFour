b9d1dcadf7e5825618c228611233e30aeb4841a6
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-08-29 20:16:09 -0300

    Atualizando Logout

--- src/test/java/com/bandtec/jobbers/controller/LogoutControllertest.java
+++ src/test/java/com/bandtec/jobbers/controller/LogoutControllertest.java
@@ -1,8 +1,32 @@
 package com.bandtec.jobbers.controller;
 
-
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
 
 public class LogoutControllertest  {
-
 	
-}
+	private LogoutController logout;
+	
+	@Before
+	public void setUp() {
+		
+		logout = new LogoutController();
+
+	}
+	
+	@Test
+	public void logoutComSucesso() {
+		
+		ResponseEntity<String> response = validadeLogout
+		
+	}
+	
+	
+	
+		
+	}
+	
+
+
+
