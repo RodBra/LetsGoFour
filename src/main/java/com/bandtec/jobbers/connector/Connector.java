0f65ec1d662527717b1ebd4e440009813f4b1b75
Wesley da Silva Batista <wesley.wbatista@tivit.com>
2019-09-05 09:52:05 -0300

    Conexão Azure

--- src/main/java/com/bandtec/jobbers/connector/Connector.java
+++ src/main/java/com/bandtec/jobbers/connector/Connector.java
@@ -1,5 +1,31 @@
 package com.bandtec.jobbers.connector;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connector {
+	public static Connection getConnection() throws ClassNotFoundException {
+		Connection conn = null;
+
+		String hostName = "irisproject.database.windows.net";
+		String dbName = "Projeto_IRIS";
+		String user = "irisProject";
+		String password = "Batatacartao00";
+		String connectionUrl = String.format(
+				"jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
+						+ "hostNameInCertificate=*.database.windows.net;loginTimeout=30;;",
+				hostName, dbName, user, password);
+
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn = DriverManager.getConnection(connectionUrl);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
 
 }
