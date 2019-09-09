efda4e8a949cb7fe3f635a32df5c621c3c6ea416
angelorafa10 <angelo.hp2012@gmail.com>
2019-09-09 20:05:35 -0300

    alterando a conexão do banco ficar funcionando

--- src/main/java/com/bandtec/jobbers/connector/Connector.java
+++ src/main/java/com/bandtec/jobbers/connector/Connector.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connector {
+	
 	public static Connection getConnection() throws ClassNotFoundException {
 		Connection conn = null;
 
@@ -14,15 +15,17 @@ public class Connector {
 		String password = "Batatacartao00";
 		String connectionUrl = String.format(
 				"jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
-						+ "hostNameInCertificate=*.database.windows.net;loginTimeout=30;;",
+						+ "hostNameInCertificate=*.database.windows.net;loginTimeout=30;",
 				hostName, dbName, user, password);
 
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			conn = DriverManager.getConnection(connectionUrl);
+			System.out.println("conexão feita com sucesso");
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
+			System.out.println("falha na conexão");
 		}
 		
 		return conn;
