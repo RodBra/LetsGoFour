b2397a82c072dc7b7e8e2610cfd6283ad740d914
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-09-20 18:46:58 -0300

    atualizacao loginDao

--- src/main/java/com/bandtec/jobbers/Dao/loginDao.java
+++ src/main/java/com/bandtec/jobbers/Dao/loginDao.java
@@ -2,6 +2,8 @@ package com.bandtec.jobbers.Dao;
 
 import com.bandtec.jobbers.controller.Credencias;
 
+
+
 public class loginDao {
 
     public boolean autenticaUsuario(Credencias credencias){
@@ -9,7 +11,10 @@ public class loginDao {
         String login = credencias.getLogin();
         String senha = credencias.getSenha();
 
-        if (login.equals("wender") && senha.equals("1234")) {
+
+        Credencias credencias1 = new Credencias("wender","1234");
+
+        if (login.equals(credencias.getLogin()) && senha.equals(credencias.getSenha())) {
             return true;
         }
         return false;
