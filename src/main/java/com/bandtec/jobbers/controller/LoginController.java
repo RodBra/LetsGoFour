5a0168d744054bc205b94b9404dd48a3984115d6
Rodrigo <rod.bra.san@gmail.com>
2019-09-24 17:33:34 -0300

    Login

--- src/main/java/com/bandtec/jobbers/controller/LoginController.java
+++ src/main/java/com/bandtec/jobbers/controller/LoginController.java
@@ -3,16 +3,19 @@ package com.bandtec.jobbers.controller;
 import com.bandtec.jobbers.Dao.loginDao;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 
-@RestController
+@Controller
 public class LoginController {
-	
-	@PostMapping("/login{nome}{senha}")
+
+	@GetMapping("/login")
+	public String login(){
+		return "login";
+	}
+
+	@PostMapping("/valida")
 	public ResponseEntity<String> validarLogin(
 			@PathVariable String nome,
 			@PathVariable String senha,
