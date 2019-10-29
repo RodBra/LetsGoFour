7003d9fadf9c6b09726b1d354a1ffb78ddd181b0
Wesley da Silva Batista <wesley.wbatista@tivit.com>
2019-10-29 12:41:15 -0300

    app.js

--- src/main/resources/jobbers/src/App.js
+++ src/main/resources/jobbers/src/App.js
@@ -4,20 +4,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css'
 
-import Login from './pages/Login/Login'
-// import PrincipalApp from './pages/PrincipalApp/PrincipalApp'
-import Cadastro from './pages/Cadastro/Cadastro'
-import CadastroPrestador from './pages/CadastroPrestador/CadastroPrestador';
-
+import PrincipalApp from './pages/PrincipalApp/PrincipalApp'
+// import Cadastro from './pages/Cadastro/Cadastro'
+// import CadastroPrestador from './pages/CadastroPrestador/CadastroPrestador';
+// import PrincipalApp from './pages/PrincipalApp/PrincipalApp';
+// import Login from './Login'
 
 export default class App extends Component {
     render() {
         return ( <
-            React.Fragment >
-
-            { /* <Login /> */ } <
-            CadastroPrestador / > { /* <PrincipalApp /> */ } { /* <Cadastro /> */ } <
-            /React.Fragment>
+            PrincipalApp / >
         )
     }
 }
