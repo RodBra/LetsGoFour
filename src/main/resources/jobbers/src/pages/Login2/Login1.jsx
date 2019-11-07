c16ad0be8f529856b978914a1379c41b1db32780
matheusor99 <matheusor99@gmail.com>
2019-11-06 21:10:47 -0300

    rotas para telas de cadastro

--- src/main/resources/jobbers/src/pages/Login2/Login1.jsx
+++ src/main/resources/jobbers/src/pages/Login2/Login1.jsx
@@ -11,15 +11,15 @@ export default class Login extends Component {
                             <div className="fadeIn first">
                                 <img src="" id="icon" alt="User Icon" />
                             </div>
-                            <form method="post">
+                            <form>
                                 <label className="fadeIn labels" id="label-usuario">Usuario</label><br />
                                 <input type="text" name="login" id="usuario" className="fadeIn second" /><br />
                                 <label className="fadeIn labels" id="label-senha">Senha</label><br />
                                 <input type="password" id="password" className="fadeIn second" name="password"></input>
-                                <input type="submit" className="fadeIn fourth" id="botao-login" value="Entrar"></input>
+                                    <input type="submit" className="fadeIn fourth" id="botao-login" value="Entrar"></input>
                             </form>
-                            <p className="fadeIn cad">para se cadastrar click <a href="www.google.com">aqui</a></p>
-                            <p className="fadeIn cad" id="cadastroPrestador">quer anunciar seus serviços <br /> cadastre-se clicando <a href="www.google.com">aqui</a></p>
+                            <p className="fadeIn cad">para se cadastrar click <a href="/register">aqui</a></p>
+                            <p className="fadeIn cad" id="cadastroPrestador">quer anunciar seus serviços <br /> cadastre-se clicando <a href="/registerPrestador">aqui</a></p>
                         </div>
                     </div>
             </Fragment>
