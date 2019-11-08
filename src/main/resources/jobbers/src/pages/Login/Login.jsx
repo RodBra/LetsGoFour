5cf5aec20e23c4d83108d1a3a43911e675479afc
matheusor99 <matheusor99@gmail.com>
2019-11-07 21:16:10 -0300

    alteracoes login

--- src/main/resources/jobbers/src/pages/Login/Login.jsx
+++ src/main/resources/jobbers/src/pages/Login/Login.jsx
@@ -3,26 +3,54 @@ import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../css/login.css'
 
+const url = 'http://localhost:8080'
 export default class Login extends Component {
+
+    state = {
+        user: {
+            usuario: '',
+            password: ''
+        }
+    }
+
+    handleChange = (event) => {
+        console.log(event.target.id);
+        
+        const user = Object.assign({}, this.state.user);
+        let  field = event.target.id;
+
+        user[field] = event.target.value;
+
+        this.setState(user);
+    }
+
+    login() {
+        axios.post(url).then(resp => {
+            console.log("oi")
+        }).catch(e => {
+            console.log("erro")
+        })
+    }
+
     render() {
         return (
             <Fragment>
-                    <div className="wrapper fadeInDown">
-                        <div id="formContent">
-                            <div className="fadeIn first">
-                                <img src="" id="icon" alt="User Icon" />
-                            </div>
-                            <form>
-                                <label className="fadeIn labels" id="label-usuario">Usuario</label><br />
-                                <input type="text" name="login" id="usuario" className="fadeIn second" /><br />
-                                <label className="fadeIn labels" id="label-senha">Senha</label><br />
-                                <input type="password" id="password" className="fadeIn second" name="password"></input>
-                                <input type="submit" className="fadeIn fourth" id="botao-login" value="Entrar"></input>
-                            </form>
-                            <p className="fadeIn cad">para se cadastrar click <a href="/register">aqui</a></p>
-                            <p className="fadeIn cad" id="cadastroPrestador">quer anunciar seus serviços <br /> cadastre-se clicando <a href="/registerPrestador">aqui</a></p>
+                <div className="wrapper fadeInDown">
+                    <div id="formContent">
+                        <div className="fadeIn first">
+                            <img src="" id="icon" alt="User Icon" />
                         </div>
+                        <form onSubmit={() => this.login()}>
+                            <label className="fadeIn labels" id="label-usuario">Usuario</label><br/>
+                            <input type="text" name="login" id="usuario" className="fadeIn second" onChange={(event) => this.handleChange(event)}/><br/>
+                            <label className="fadeIn labels" id="label-senha">Senha</label><br/>
+                            <input type="password" id="password" className="fadeIn second" name="password" onChange={(event) => this.handleChange(event)}></input>
+                            <input type="submit" className="fadeIn fourth" id="botao-login" value="Entrar"></input>
+                        </form>
+                        <p className="fadeIn cad">para se cadastrar click <a href="/register">aqui</a></p>
+                        <p className="fadeIn cad" id="cadastroPrestador">quer anunciar seus serviços <br/> cadastre-se clicando <a href="/registerPrestador">aqui</a></p>
                     </div>
+                </div>
             </Fragment>
         )
     }
